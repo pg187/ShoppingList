3f23933c006f237d862eafd982c04d40f4f966b9
Pragya <prog187.pg@gmail.com>
2019-10-16 20:22:10 -0400

    Ui Design done

--- ShoppingList/ViewController.swift
+++ ShoppingList/ViewController.swift
@@ -9,12 +9,58 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    private var value: String = ""
+    private var result: Int = 0
+    @IBOutlet weak var CancelButton: UIButton!
+    @IBOutlet weak var SaveButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.CancelButton.layer.cornerRadius = 5
+        self.SaveButton.layer.cornerRadius = 5
     }
 
-
+    @IBOutlet weak var item1quantity: UITextField!
+    @IBAction func increaseButton1(_ sender: Any) {
+        value = item1quantity.text!
+        result = Int(value)! + 1
+        item1quantity.text = String(result)
+    }
+    
+    @IBAction func decreaseButton1(_ sender: Any) {
+        value = item1quantity.text!
+        if(value == "1")
+        {
+            item1quantity.text = "1"
+        }
+        else
+        {
+            result = Int(value)! - 1
+            item1quantity.text = String(result)
+        }
+        value = ""
+    }
+    
+    
+    @IBOutlet weak var item2quantity: UITextField!
+    
+    @IBAction func decreaseButton2(_ sender: Any) {
+        value = item1quantity.text!
+        result = Int(value)! + 1
+        item1quantity.text = String(result)
+    }
+    
+    @IBAction func increaseButton2(_ sender: Any) {
+        value = item1quantity.text!
+        if(value == "1")
+        {
+            item1quantity.text = "1"
+        }
+        else
+        {
+            result = Int(value)! - 1
+            item1quantity.text = String(result)
+        }
+        value = ""
+    }
 }
-
